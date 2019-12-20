c4e469239da717dafcf9608c311d78bf291e4b84
YiazmatXII-afk <59088613+YiazmatXII-afk@users.noreply.github.com>
2019-12-20 13:44:04 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "bot_kistune",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "Yiazmat",
+  "license": "ISC",
+  "dependencies": {
+    "discord.js": "^11.5.1",
+    "fs": "0.0.1-security",
+    "superagent": "^5.1.2"
+  }
+}
